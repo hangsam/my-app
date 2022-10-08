6450e5b2d5eb3f3500510af3a21fdfd753bafa7b
Samsuhang Nembang <85781143+hangsam@users.noreply.github.com>
2022-10-08 10:46:09 +0545

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="outer">
+        <div className="inner">
+          <Register />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
